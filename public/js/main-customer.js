f7ba5ee4a69a4362c111c02f720acaf7dfbfb234
hipzi <zahramillah582@gmail.com>
2020-10-26 16:08:53 +0700

    web design for customer

---
+++ public/js/main-customer.js
@@ -0,0 +1,44 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("movetop").style.display = "block";
+    } else {
+        document.getElementById("movetop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+$('#customerhnyCarousel').carousel({
+    interval: 5000
+});
+
+$(document).ready(function () {
+    $('.popup-with-zoom-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+    });
+});
+
+$(function () {
+    $('.navbar-toggler').click(function () {
+        $('body').toggleClass('noscroll');
+    })
+});
+
+console.log('bismillah');
---
+++ resources/views/layouts/customer.blade.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Bismillah</title>
+	
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/style-customer.css') }}" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Icon -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Fonts -->
+    <link href="//fonts.googleapis.com/css?family=Oswald:300,400,500,600&display=swap" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,900&display=swap" rel="stylesheet">
+	
+</head>
+<body>
+<!--w3l-banner-slider-main-->
+<section class="w3l-banner-slider-main">
+	<div class="top-header-content">
+		<header class="tophny-header">
+			<div class="container-fluid">
+				<div class="top-right-strip row">
+					<!--/left-->
+					<div class="top-hny-left-content col-lg-6 pl-lg-0">
+						<a class="navbar-brand" href="index.html">
+							Soft<span class="lohny">W</span>are</a>
+					</div>
+					<!--//left-->
+					<!--/nav-->
+					<nav class="navbar navbar-expand-lg navbar-light ml-auto">
+						<div class="container-fluid serarc-fluid">
+							<button class="navbar-toggler" type="button" data-toggle="collapse"
+								data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+								aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon fa fa-bars" style="color:#d8cdb1"> </span>
+							</button>
+							<div class="collapse navbar-collapse" id="navbarSupportedContent">
+								<ul class="navbar-nav ml-auto">
+									<li class="nav-item active">
+										<a class="nav-link" href="index.html">Home</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">About</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">Shop</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">Login</a>
+									</li>
+								</ul>
+
+							</div>
+						</div>
+					</nav>
+					<!--//nav-->
+				</div>
+			</div>
+		</header>
+</section>
+<br>
+<br>
+<br>
+<!-- Content -->
+<main>
+    @yield('content')
+</main>
+<!-- End Content -->
+
+<section class="w3l-footer-22">
+    <!-- footer-22 -->
+    <div class="footer-hny">
+        <div class="container">
+            <div class="below-section row">
+                <div class="columns col-lg-6" style="margin-top:-5px">
+                    <ul class="jst-link">
+                        <li><a href="#">Privacy Policy </a> </li>
+                        <li><a href="#">Term of Service</a></li>
+                        <li><a href="contact.html">Customer Care</a> </li>
+                    </ul>
+                </div>
+                <div class="columns col-lg-6 text-lg-right" style="margin-top:10px">
+                    <p>© Webpro - Midterm
+                    </p>
+                </div>
+                <button onclick="topFunction()" id="movetop" title="Go to top">
+                    <span class="fa fa-angle-double-up"></span>
+                </button>
+            </div>
+        </div>
+    </div>
+	<!-- //titels-5 -->
+</section>
+<script src="{{ asset('js/jquery.magnific-popup.js') }}" defer></script>
+<script src="{{ asset('js/main-customer.js') }}"></script>
+
+<!-- Bootstrap -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</body>
+</html>
